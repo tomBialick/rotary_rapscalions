09e7e8730c52c8c098afb1968d5ac262c133287e
tom <tb7150@vt.email>
2022-02-25 18:01:37 -0500

    LCD display and variable length phone number support

--- software/rotary_read.ino/rotary_read.ino.ino
+++ software/rotary_read.ino/rotary_read.ino.ino
@@ -1,4 +1,8 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 const int ROTARY_DIAL = A3;
+const int CALL_PIN = 7;
 const int HIGH_THRESH = 400; 
 const int LOW_THRESH = 25; 
 const int PULSE_TIME = 50; //ms
@@ -9,7 +13,8 @@ enum PhoneStatus {
   null_state,
   on_hook,
   off_hook,
-  dialing
+  dialing,
+  calling
 };
 
 PhoneStatus phone; 
@@ -19,7 +24,8 @@ int lastTimeHigh;
 int lastTimeLow;
 int lastTimePulse;
 bool pulseCountedFlag;
-int lastDialed;
+char* phoneNumber;
+int digitCounter;
 
 void setup() {
   Serial.begin(9600);
@@ -30,7 +36,13 @@ void setup() {
   lastTimeHigh = 0;
   lastTimeLow = 0;
   pulseCountedFlag = false;
-  lastDialed = -1;
+
+  lcd.begin(16, 2);
+
+  pinMode(CALL_PIN, INPUT);
+
+  phoneNumber = (char*) malloc(17 * sizeof(char));
+  digitCounter = 0;
 }
 
 void nullState(int analRead) {
@@ -48,6 +60,11 @@ void nullState(int analRead) {
 
 void onHook(int analRead) {
   pulseBuffer = 0;
+  if (digitCounter != 0) {
+    free(phoneNumber);
+    digitCounter = 0;
+    phoneNumber = (char*) malloc(17 * sizeof(char));
+  }
   pulseCountedFlag = false;
   if (analRead >= HIGH_THRESH && (lastTimeLow  + HOOK_CHANGE_THRESH) <= currTime) {
     phone = off_hook;
@@ -56,9 +73,10 @@ void onHook(int analRead) {
   else if(analRead <= LOW_THRESH) {
     lastTimeLow = millis();
   }
+  lcd.clear();
 }
 
-void offHook(int analRead) {
+void offHook(int analRead, int digiRead) {
   pulseCountedFlag = false;
   if (analRead <= LOW_THRESH && (lastTimeHigh + HOOK_CHANGE_THRESH) <= currTime) {
     phone = on_hook;
@@ -69,6 +87,12 @@ void offHook(int analRead) {
     pulseBuffer++;
     lastTimeLow = millis();
     pulseCountedFlag = true;
+  }
+  else if (digiRead == HIGH) {
+    phone = calling;
+    if (analRead >= HIGH_THRESH) {
+      lastTimeHigh = millis();
+    }
   }
   else if (analRead >= HIGH_THRESH) {
     lastTimeHigh = millis();
@@ -90,8 +114,11 @@ void dialingState(int analRead) {
   else if (analRead >= HIGH_THRESH && (lastTimeLow + PAUSE_TIME) < currTime ) {
     phone = off_hook;
     lastTimeHigh = millis();
-    lastDialed = pulseBuffer;
-    Serial.println(lastDialed); //call when ready
+    if (digitCounter < 16) {
+      printDialed(pulseBuffer);
+      phoneNumber[digitCounter] = pulseBuffer == 10 ? '0': pulseBuffer + '0';
+      digitCounter++;
+    }
     pulseBuffer = 0;
   }
   if(analRead <= LOW_THRESH) {
@@ -103,10 +130,35 @@ void dialingState(int analRead) {
   }
 }
 
+//will need to pass the phone number pointer
+void callingState(int analRead) {
+  phoneNumber[digitCounter] = '\0';
+  lcd.setCursor(0, 1);
+  lcd.print("Calling...");
+  if (analRead >= HIGH_THRESH) {
+    lastTimeHigh = millis();
+  }
+  else if (analRead <= LOW_THRESH && (lastTimeHigh + HOOK_CHANGE_THRESH) <= currTime) {
+    phone = on_hook;
+    lastTimeLow = millis();
+  }
+  if (digitCounter != 0) {
+    Serial.println(phoneNumber);
+    digitCounter = 0;
+    free(phoneNumber);
+  }
+}
+
+void printDialed(int lastDialed) {
+  int digit = lastDialed == 10 ? 0: lastDialed;
+  lcd.print(digit);
+}
+
 void loop() {
   int analRead = analogRead(ROTARY_DIAL);
+  int digiRead = digitalRead(CALL_PIN);
   currTime = millis();
-
+  
   switch(phone) {
     case null_state:
       nullState(analRead);
@@ -115,10 +167,13 @@ void loop() {
       onHook(analRead);
       break;
     case off_hook:
-      offHook(analRead);
+      offHook(analRead, digiRead);
       break;
     case dialing:
       dialingState(analRead);
+      break;
+    case calling:
+      callingState(analRead);
       break;
   }
 }
