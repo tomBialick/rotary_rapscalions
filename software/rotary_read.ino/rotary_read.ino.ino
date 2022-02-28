a730507969b57378ea7ef15f25710843fbff435a
tom <tb7150@vt.email>
2022-02-27 23:09:00 -0500

    added ringing

--- software/rotary_read.ino/rotary_read.ino.ino
+++ software/rotary_read.ino/rotary_read.ino.ino
@@ -3,26 +3,42 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int ROTARY_DIAL = A3;
 const int CALL_PIN = 7;
+const int RING_PIN = 8;
+const int VSS_ONE = 9;
+const int VSS_TWO = 10;
+const int PIEZO_PIN = 6;
 const int HIGH_THRESH = 400; 
 const int LOW_THRESH = 25; 
 const int PULSE_TIME = 50; //ms
 const int PAUSE_TIME = 50; //ms
 const int HOOK_CHANGE_THRESH = 250; //ms
+const int RING_PERIOD = 50; //ms
+const int RING_LENGTH = 1000; //ms
 
 enum PhoneStatus {
   null_state,
   on_hook,
   off_hook,
   dialing,
-  calling
+  calling,
+  ringing
+};
+
+enum RingStatus {
+  left,
+  right,
+  pause
 };
 
 PhoneStatus phone; 
+RingStatus ring;
 int pulseBuffer;
-int currTime;
-int lastTimeHigh;
-int lastTimeLow;
-int lastTimePulse;
+unsigned long currTime;
+unsigned long lastTimeHigh;
+unsigned long lastTimeLow;
+unsigned long lastTimePulse;
+unsigned long ringTime;
+unsigned long pauseTime;
 bool pulseCountedFlag;
 char* phoneNumber;
 int digitCounter;
@@ -31,15 +47,22 @@ void setup() {
   Serial.begin(9600);
   
   phone = null_state;
+  ring = left;
   pulseBuffer = 0;
   currTime = 0;
   lastTimeHigh = 0;
   lastTimeLow = 0;
+  ringTime = 0;
+  pauseTime = 0;
   pulseCountedFlag = false;
 
   lcd.begin(16, 2);
 
   pinMode(CALL_PIN, INPUT);
+  pinMode(RING_PIN, INPUT);
+  pinMode(VSS_ONE, OUTPUT);
+  pinMode(VSS_TWO, OUTPUT);
+  pinMode(PIEZO_PIN, OUTPUT);
 
   phoneNumber = (char*) malloc(17 * sizeof(char));
   digitCounter = 0;
@@ -58,7 +81,7 @@ void nullState(int analRead) {
   }
 }
 
-void onHook(int analRead) {
+void onHook(int analRead, int digiReadRing) {
   pulseBuffer = 0;
   if (digitCounter != 0) {
     free(phoneNumber);
@@ -70,13 +93,21 @@ void onHook(int analRead) {
     phone = off_hook;
     lastTimeHigh = millis();
   }
+  else if (digiReadRing == HIGH) {
+    phone = ringing;
+    ring = left;
+    pauseTime = millis();
+    if (analRead >= HIGH_THRESH) {
+      lastTimeHigh = millis();
+    }
+  }
   else if(analRead <= LOW_THRESH) {
     lastTimeLow = millis();
   }
   lcd.clear();
 }
 
-void offHook(int analRead, int digiRead) {
+void offHook(int analRead, int digiReadCall) {
   pulseCountedFlag = false;
   if (analRead <= LOW_THRESH && (lastTimeHigh + HOOK_CHANGE_THRESH) <= currTime) {
     phone = on_hook;
@@ -88,7 +119,7 @@ void offHook(int analRead, int digiRead) {
     lastTimeLow = millis();
     pulseCountedFlag = true;
   }
-  else if (digiRead == HIGH) {
+  else if (digiReadCall == HIGH) {
     phone = calling;
     if (analRead >= HIGH_THRESH) {
       lastTimeHigh = millis();
@@ -132,9 +163,14 @@ void dialingState(int analRead) {
 
 //will need to pass the phone number pointer
 void callingState(int analRead) {
-  phoneNumber[digitCounter] = '\0';
-  lcd.setCursor(0, 1);
-  lcd.print("Calling...");
+  if (digitCounter != 0) {
+    phoneNumber[digitCounter] = '\0';
+    lcd.setCursor(0, 1);
+    lcd.print("Calling...");
+    Serial.println(phoneNumber);
+    digitCounter = 0;
+    free(phoneNumber);
+  }
   if (analRead >= HIGH_THRESH) {
     lastTimeHigh = millis();
   }
@@ -142,10 +178,58 @@ void callingState(int analRead) {
     phone = on_hook;
     lastTimeLow = millis();
   }
-  if (digitCounter != 0) {
-    Serial.println(phoneNumber);
-    digitCounter = 0;
-    free(phoneNumber);
+}
+
+void ringingState(int analRead) {
+  if (analRead >= HIGH_THRESH && (lastTimeLow  + HOOK_CHANGE_THRESH) <= currTime) {
+    phone = off_hook;
+    lastTimeHigh = millis();
+    digitalWrite(VSS_ONE, LOW);
+    digitalWrite(VSS_TWO, LOW);
+    noTone(PIEZO_PIN);
+  }
+  Serial.println(ring);
+  switch (ring) {
+    case pause:
+      if ((currTime - pauseTime) <= RING_LENGTH) {
+        digitalWrite(VSS_ONE, LOW);
+        digitalWrite(VSS_TWO, LOW);
+        noTone(PIEZO_PIN);
+      }
+      else {
+        pauseTime = millis();
+        ring = left;
+      }
+      break;
+    case left:
+    case right:
+      if ((currTime - pauseTime) <= RING_LENGTH) {
+        if ((currTime - ringTime) >= RING_PERIOD/2) {
+          ringTime = currTime;
+          tone(PIEZO_PIN, 250);
+          switch(ring) {
+            case left:
+              digitalWrite(VSS_ONE, HIGH);
+              digitalWrite(VSS_TWO, LOW);
+              ring = right;
+              break;
+            case right:
+              digitalWrite(VSS_ONE, LOW);
+              digitalWrite(VSS_TWO, HIGH);
+              ring = left;
+              break;
+          }
+        }
+      }
+      else {
+        pauseTime = millis();
+        ring = pause;
+      }
+      break;
+  }
+  
+  if(analRead <= LOW_THRESH) {
+    lastTimeLow = millis();
   }
 }
 
@@ -156,24 +240,28 @@ void printDialed(int lastDialed) {
 
 void loop() {
   int analRead = analogRead(ROTARY_DIAL);
-  int digiRead = digitalRead(CALL_PIN);
+  int digiReadCall = digitalRead(CALL_PIN);
+  int digiReadRing = digitalRead(RING_PIN);
   currTime = millis();
   
-  switch(phone) {
+  switch (phone) {
     case null_state:
       nullState(analRead);
       break;
     case on_hook:
-      onHook(analRead);
+      onHook(analRead, digiReadRing);
       break;
     case off_hook:
-      offHook(analRead, digiRead);
+      offHook(analRead, digiReadCall);
       break;
     case dialing:
       dialingState(analRead);
       break;
     case calling:
       callingState(analRead);
+      break;
+    case ringing:
+      ringingState(analRead);
       break;
   }
 }
